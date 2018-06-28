1157e41db426b6a4dde6655e03f61d3bf0e0e8fe
george <georgeheron@georges-MacBook-Pro.local>
2018-06-27 23:14:49 -0500

    Write basic instructions to be used in later days during exercides.

---
+++ javascript/intro/app.js
@@ -0,0 +1,60 @@
+function sample1(){
+  var element = document.getElementById('sample1');
+  var sentence = "Nory was a Catholic because her mother was a Catholic, and Nory’s mother was a Catholic because her father was a Catholic, and her father was a Catholic because his mother was a Catholic, or had been.";
+  element.textContent = sentence;
+}
+
+
+function sample2(){
+  var element = document.getElementById('sample2');
+  var sentence = "I felt extravagant because I saw the others were extravagant and because I knew I should feel extravagant, but I wasn’t really extravagant.";
+  element.textContent = sentence;
+}
+
+function sample3(){
+  var element = document.getElementById('sample3');
+  var sentence = "But good morning! Good morning to ye and thou! I’d say to all my patients, because I was the worse of the hypocrites, of all the hypocrites, the cruel and phony hypocrites, I was the very worst.";
+  element.textContent = sentence;
+}
+
+function sample4(){
+  var element = document.getElementById('sample4');
+  var sentence = "So I said yes to Thomas Clinton and later thought that I had said yes to mother and later still realized I had said yes only to Thomas Clinton.";
+  element.textContent = sentence;
+}
+
+function sample5(){
+  var element = document.getElementById('sample5');
+  var sentence = "Because they are djinns and I'm not a djinn, I want to become a djinn like them.";
+  element.textContent = sentence;
+}
+
+function sample6(){
+  var element = document.getElementById('sample6');
+  var sentence = "The depressed person was in terrible and unceasing emotional pain, and the impossibility of sharing or articulating this pain was itself a component of the pain and a contributing factor in its essential horror.";
+  element.textContent = sentence;
+}
+
+function sample7(){
+  var element = document.getElementById('sample7');
+  var sentence = "Paranoids are not paranoid because they’re paranoid, but because they keep putting themselves, stupid idiots, deliberately into paranoid situations.";
+  element.textContent = sentence;
+}
+
+function sample8(){
+  var element = document.getElementById('sample8');
+  var sentence = "She was young the way an actual young person is young.";
+  element.textContent = sentence;
+}
+
+
+
+
+sample1();
+sample2();
+sample3();
+sample4();
+sample5();
+sample6();
+sample7();
+sample8();
